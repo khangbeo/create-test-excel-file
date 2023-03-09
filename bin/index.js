c60931b30a9f3b3068088687c2880700d8c03f7d
Anthony <khangbeo2012@gmail.com>
2023-03-09 13:21:32 -0800

    moved some code blocks into functions

--- bin/index.js
+++ bin/index.js
@@ -9,42 +9,53 @@ const rl = require("readline-sync");
 const currentDirectory = path.join(__dirname).replace("/bin", "");
 
 // creates a folder if it doesn't exist already
-const folderName = `${currentDirectory}/test`;
-createFolder(folderName);
+createFolder(`${currentDirectory}/test`);
 
 // grab all files with .xlsx extension
 const files = fs
   .readdirSync(currentDirectory)
   .filter((fn) => fn.endsWith(".xlsx"));
-
 console.log(`You have ${files.length} excel files`);
 const fileIndex = rl.keyInSelect(files, "Which file do you want to use? ");
-console.log(files[fileIndex]);
+if (fileIndex === -1) {
+  console.log("Exiting the program");
+  return;
+}
+const foundFile = files[fileIndex];
 
-// start extracting first row from workbook
-const workbook = XLSX.readFile(files[fileIndex]);
-const worksheet = workbook.Sheets["Sheet1"];
-const jsonSheet = XLSX.utils.sheet_to_json(worksheet);
-const newSheet = XLSX.utils.json_to_sheet(new Array(jsonSheet[0]));
 let newFilename = "";
+const oldFilename = foundFile.replace(".xlsx", "");
+const changedFilename = cleanUpName(foundFile);
 
-console.log(
-  "\nWhat do you want the new file name to be? (case doesn't matter)"
-);
-rl.promptLoop((input) => {
-  return input.length > 0
-    ? (newFilename = input)
-    : console.log("You need to enter something!");
-});
+createWorkbook(foundFile);
 
-// start new workbook and save in test directory
-const newWorkbook = XLSX.utils.book_new();
-XLSX.utils.book_append_sheet(newWorkbook, newSheet, "Sheet1");
-XLSX.writeFile(newWorkbook, `${currentDirectory}/test/${newFilename}-test.csv`);
-console.log(
-  `\nYour new file "${newFilename}" is saved in this folder: ${currentDirectory}/test/${newFilename}.csv\n`
-);
-console.log("Process finished!");
+function createWorkbook(foundFile) {
+  // start extracting first row from workbook
+  const workbook = XLSX.readFile(`${currentDirectory}/${foundFile}`);
+  const worksheet = workbook.Sheets["Sheet1"];
+  const jsonSheet = XLSX.utils.sheet_to_json(worksheet);
+  const newSheet = XLSX.utils.json_to_sheet(new Array(jsonSheet[0]));
+
+  console.log(
+    `\nWhat do you want the new file name to be? (case doesn't matter). 
+  Type same if you want to keep the old name.
+  Type update if you want the old name to be updated like this "file-name-lol.xlsx"`
+  );
+
+  rl.promptLoop(askForName);
+
+  // start new workbook and save in test directory
+  const newWorkbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(newWorkbook, newSheet, "Sheet1");
+  XLSX.writeFile(
+    newWorkbook,
+    `${currentDirectory}/test/${newFilename}-test.csv`
+  );
+  console.log(
+    `\nYour new file "${newFilename}" is saved in this folder: ${currentDirectory}/test/${newFilename}.csv\n`
+  );
+  console.log("Process finished!");
+}
 
 function createFolder(name) {
   try {
@@ -60,7 +71,29 @@ function createFolder(name) {
   }
 }
 
+function askForName(input) {
+  if (input.length === 0) {
+    console.log("You need to enter something!");
+  }
+  switch (input) {
+    case "same":
+      newFilename = oldFilename;
+      break;
+    case "update":
+      newFilename = changedFilename;
+      break;
+    default:
+      newFilename = input.trim().toLowerCase();
+  }
+  return true;
+}
+
+function cleanUpName(name) {
+  return name.replace(".xlsx", "").toLowerCase().split(" ").join("-");
+}
 /**
+ * script works but currently very messy
+ *     need to clean up functions and reorganize logic
  *
  * Features to add
  * check for existing file
@@ -73,4 +106,5 @@ function createFolder(name) {
  * add a prompt loop
  * maybe get input with html form instead of terminal
  *     if html form, i don't need to get current directory, just drag and drop file then download the result
+ *
  */
