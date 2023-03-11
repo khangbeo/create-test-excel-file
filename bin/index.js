09ecc1488c150c162c5362de78e9cef6e6e812dd
Anthony <khangbeo2012@gmail.com>
2023-03-10 21:28:09 -0800

    updated code to look for test-csv-files

--- bin/index.js
+++ bin/index.js
@@ -6,8 +6,9 @@ const rl = require("readline-sync");
 
 function main() {
   const currentDirectory = getCurrentDirectory();
-  createTestFolder(`${currentDirectory}/test`);
+  createTestFolder(`${currentDirectory}/test-csv-files`);
   const foundFile = locateFiles(currentDirectory, "xlsx");
+  console.log(foundFile);
   if (!foundFile) return;
   const data = parseExcelFile(currentDirectory, foundFile);
   const newName = askForName(foundFile);
@@ -46,10 +47,10 @@ function writeToCsvFile(newSheet, currentDirectory, newFilename) {
     XLSX.utils.book_append_sheet(newWorkbook, newSheet, "Sheet1");
     XLSX.writeFile(
       newWorkbook,
-      `${currentDirectory}/test/${newFilename}-test.csv`
+      `${currentDirectory}/test-csv-files/${newFilename}-test.csv`
     );
     console.log(
-      `\nYour new file "${newFilename}" is saved in this folder: ${currentDirectory}/test/${newFilename}.csv\n`
+      `\nYour new file "${newFilename}" is saved in this folder: ${currentDirectory}/test-csv-files/${newFilename}.csv\n`
     );
     console.log("Process finished!");
   } catch (e) {
@@ -77,7 +78,7 @@ function askForName(foundFile) {
   console.log(
     `\nWhat do you want the new file name to be? (case doesn't matter). 
       Type same if you want to keep the old name.
-      Type update if you want the old name to be updated like this "file-name-lol.xlsx"`
+      Type update if you want the old name to be updated like this "file-name-lol.csv"`
   );
 
   rl.promptLoop((input) => {
@@ -110,18 +111,12 @@ function getCurrentDirectory() {
 
 main();
 
-/* script works but currently very messy
- *     need to clean up functions and reorganize logic
+/*
  *
  * Features to add
  * check for existing file
  *     if exists then ask user to enter a new name
  *     keep looping until file name is unique
- * first prompt makes an array and asks user to choose, need to change it so user can enter the file name instead of array index
- * or just make sure there's only one file in the folder and prevent user from using script if there's multiple files
- *
- * add error handler for wrong input maybe with regex, case-insensitive input
- * add a prompt loop
  * maybe get input with html form instead of terminal
  *     if html form, i don't need to get current directory, just drag and drop file then download the result
  *
